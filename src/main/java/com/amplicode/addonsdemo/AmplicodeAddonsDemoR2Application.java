64c88f9f07896d8d8fce2b9b184896b71980bffb
subbotin <a.subbotin@haulmont.com>
2021-11-24 18:13:14 +0400

    Add JpaAuditing on sample project instead of addons

--- src/main/java/com/amplicode/addonsdemo/AmplicodeAddonsDemoR2Application.java
+++ src/main/java/com/amplicode/addonsdemo/AmplicodeAddonsDemoR2Application.java
@@ -3,9 +3,11 @@ package com.amplicode.addonsdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.envers.repository.config.EnableEnversRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableEnversRepositories({"com.amplicode"})
 @SpringBootApplication
+@EnableJpaAuditing
 public class AmplicodeAddonsDemoR2Application {
 
     public static void main(String[] args) {
