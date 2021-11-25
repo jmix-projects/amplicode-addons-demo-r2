42661c923e2e66550638273598620924710e2ba2
subbotin <a.subbotin@haulmont.com>
2021-11-25 11:17:52 +0400

    - Fix GraphQL configuration

--- src/main/java/com/amplicode/addonsdemo/config/GraphQLConfiguration.java
+++ src/main/java/com/amplicode/addonsdemo/config/GraphQLConfiguration.java
@@ -4,25 +4,31 @@ import com.amplicode.addonsdemo.graphql.ErrorType;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.execution.DataFetcherResult;
+import io.leangen.graphql.ExtensionProvider;
+import io.leangen.graphql.GeneratorConfiguration;
 import io.leangen.graphql.execution.InvocationContext;
 import io.leangen.graphql.execution.ResolverInterceptor;
+import io.leangen.graphql.execution.ResolverInterceptorFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDeniedException;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class GraphQLConfiguration {
 
-    //TODO: access denied bugfix
-//    @Bean
-//    public ExtensionProvider<GeneratorConfiguration, ResolverInterceptorFactory> resolverInterceptorFactoryProvider(ApplicationContext applicationContext) {
-//        return (config, defaults) -> defaults.append(params -> getResolverInterceptors());
-//    }
+    @Bean
+    public ExtensionProvider<GeneratorConfiguration, ResolverInterceptorFactory> resolverInterceptorFactoryProvider() {
+        return (config, defaults) -> defaults.append(params -> getResolverInterceptors());
+    }
 
-//    protected List<ResolverInterceptor> getResolverInterceptors() {
-//        return Arrays.asList(new AccessDeniedResolverInterceptor(), new ValidationInterceptor());
-//    }
+    protected List<ResolverInterceptor> getResolverInterceptors() {
+        return Arrays.asList(new AccessDeniedResolverInterceptor());
+    }
 
-    //TODO: access denied bugfix
     private static class AccessDeniedResolverInterceptor implements ResolverInterceptor {
         @Override
         public Object aroundInvoke(InvocationContext context, Continuation continuation) throws Exception {
@@ -39,50 +45,4 @@ public class GraphQLConfiguration {
             }
         }
     }
-
-    //TODO: Bean validation: Add constraints into GraphQL error (doesn't required for MVP)
-//    private static class ValidationInterceptor implements ResolverInterceptor {
-//        @Override
-//        public Object aroundInvoke(InvocationContext context, Continuation continuation) throws Exception {
-//            try {
-//                return continuation.proceed(context);
-//            } catch (ConstraintViolationException e) {
-//                GraphQLError error = GraphqlErrorBuilder.newError()
-//                        .errorType(graphql.ErrorType.ValidationError)
-//                        .extensions(getExtensions(e.getConstraintViolations()))
-//                        .message("Bean validation error")
-//                        .build();
-//                return DataFetcherResult.newResult()
-//                        .error(error)
-//                        .build();
-//            }
-//        }
-
-//        private Map<String, Object> getExtensions(Set<ConstraintViolation<?>> constraintViolations) {
-//            return Map.of("constraintViolations", constraintViolations.stream()
-//                    .map(this::composeErrorExtension)
-//                    .collect(Collectors.toList()));
-//        }
-
-//        private Map<String, Object> composeErrorExtension(ConstraintViolation<?> constraintViolation) {
-//            Map<String, Object> errorMap = new HashMap<>();
-//
-//            errorMap.put("messageTemplate", constraintViolation.getMessageTemplate());
-//            errorMap.put("message", constraintViolation.getMessage());
-//            errorMap.put("path", composePath(constraintViolation));
-//            errorMap.put("invalidValue", constraintViolation.getInvalidValue().toString());
-//
-//            return errorMap;
-//        }
-
-//        private String composePath(ConstraintViolation<?> constraintViolation) {
-//            GraphQLApi annotation = AnnotationUtils.findAnnotation(constraintViolation.getRootBeanClass(), GraphQLApi.class);
-//            if (annotation != null) {
-//                String propertyPath = constraintViolation.getPropertyPath().toString();
-//                return propertyPath.substring(propertyPath.indexOf(".") + 1);
-//            } else {
-//                return constraintViolation.getPropertyPath().toString();
-//            }
-//        }
-//    }
 }
